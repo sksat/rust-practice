a4a5c57104fe429742fa7a39deca93dad1a49c61
sksat <sksat.tec@gmail.com>
2018-03-28 00:20:17 +0900

    [UPDATE] generate secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand; // randを使う
+
 use std::io;
+use rand::Rng; // gen_rangeメソッドが動作するためにトレイトがスコープにある必要がある
 
 fn main() { // 関数宣言．引数の型を書いていないので空のタプルとして扱われる
     println!("Guess the number!"); // println!は文字列表示マクロ
+
+    let secret_number = rand::thread_rng().gen_range(1,101); // 現在いるスレッドにローカルな乱数生成器のコピーを取得，範囲を指定して乱数を生成
+
+    println!("The secret number is {}", secret_number);
 
     println!("Please input your guess.");
 
